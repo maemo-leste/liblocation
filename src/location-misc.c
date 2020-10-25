fa84f9ee06dd955e89b7b6c7e786fdb79d889106
parazyd <parazyd@dyne.org>
2020-10-25 18:14:40 +0100

    Try using a portable way to find the .so.

--- src/location-misc.c
+++ src/location-misc.c
@@ -4,12 +4,14 @@
 
 void location_make_resident()
 {
-	GModule *module;
+	gchar *module_path;
 	const gchar *err;
 
-	/* TODO: See if there is some define for the correct location in
-	 * autoconf */
-	module = g_module_open("/usr/lib/liblocation.so.0", 0);
+	module_path = g_module_build_path(NULL, "liblocation");
+	if (!module_path)
+		return;
+
+	GModule *module = g_module_open(module_path, 0);
 	if (!module) {
 		err = g_module_error();
 		g_log("liblocation", G_LOG_LEVEL_CRITICAL,
