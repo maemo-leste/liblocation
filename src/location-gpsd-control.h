7b0a2da9e4f86be5cb6c5a870b90f94d8ee8d990
parazyd <parazyd@dyne.org>
2020-11-05 00:34:56 +0100

    Update license headers.

--- src/location-gpsd-control.h
+++ src/location-gpsd-control.h
@@ -1,17 +1,20 @@
 /*
+ * Copyright (c) 2020 Ivan J. <parazyd@dyne.org>
+ *
  * This file is part of liblocation
  *
- * Copyright (C) 2007 Nokia Corporation. All rights reserved.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Contact: Quanyi Sun <Quanyi.Sun@nokia.com>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * This software, including documentation, is protected by copyright
- * controlled by Nokia Corporation. All rights are reserved.  Copying,
- * including reproducing, storing, adapting or translating, any or all
- * of this material requires the prior written consent of Nokia
- * Corporation. This material also contains confidential information
- * which may not be disclosed to others without the prior written
- * consent of Nokia.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef __LOCATION_GPSD_CONTROL_H__
